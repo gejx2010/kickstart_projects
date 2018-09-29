0c81174e456530f6d7e267309c12282f6d968ceb
gejunxiang <gejunxiang@sogou-inc.com>
2018-09-29 20:26:10 +0800

    Add base_model to inherit from

---
+++ base_model/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+#define LARGE 200001
+#define COMPILE false
+#define TESTTIME false
+
+int ini_array[LARGE], sum_array[LARGE];
+
+void printf_array(void* a, int num, char* name, char* type="int") {
+  printf("Begin print content of %s:\n", name);
+  int* b = NULL;
+  long long* c = NULL;
+  if (strcmp(type, "int") == 0) b = (int*)a; 
+  else c = (long long*)a;
+  for (int k = 0; k < num; k++) {
+    if (strcmp(type, "int") == 0) printf("%d ", b[k]);
+    else printf("%lld ", c[k]);
+  }
+  printf("\nAfter print content of %s.\n", name);
+}
+
+int main(int argc, char** argv) {
+  char* def_ifn = "large.in";
+  char* def_ofn = "large.out";
+  if (1 < argc) def_ifn = argv[1];
+  if (2 < argc) def_ofn = argv[2];
+  freopen(def_ifn, "r", stdin);
+  freopen(def_ofn, "w", stdout);
+  int T = 0;
+  scanf("%d", &T);
+  if (COMPILE) printf("Get T: %d\n", T);
+  int i = 0;
+  while (i++ < T) {
+    clock_t st = clock();
+    if (TESTTIME) printf("With Case %d.\n", i);
+    clock_t rt = clock();
+    if (TESTTIME) printf("Read in data takes time: %f seconds\n", ((float)(rt - st)) / CLOCKS_PER_SEC);
+    if (COMPILE) printf_array(ini_array, 0, "ini_array", "int");
+    printf("Case #%d: \n", i);
+  }
+  return 0;
+}
